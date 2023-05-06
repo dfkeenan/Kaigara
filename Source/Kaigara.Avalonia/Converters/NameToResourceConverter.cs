9c04e020ef2d97c249acda19b81f51b96a0ee78c
dfkeenan <dfkeenan@gmail.com>
2023-05-06 22:05:09 +1000

    Change NameToResourceConverter

--- Source/Kaigara.Avalonia/Converters/NameToResourceConverter.cs
+++ Source/Kaigara.Avalonia/Converters/NameToResourceConverter.cs
@@ -2,16 +2,24 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Data.Converters;
+using Avalonia.Markup.Xaml.XamlIl.Runtime;
 
 namespace Kaigara.Avalonia.Converters;
 
 public class NameToResourceConverter : IValueConverter
 {
-    public static NameToResourceConverter Instance { get; } = new NameToResourceConverter();
+    private IResourceHost? resourceHost;
+
+    private IResourceHost? ResourceHost => resourceHost ?? Application.Current;
+
+    public NameToResourceConverter(IResourceHost? resourceHost = null)
+    {
+        this.resourceHost = resourceHost;
+    }
 
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is string name && Application.Current?.TryFindResource(name, out var resource) == true)
+        if (value is string name && ResourceHost?.TryFindResource(name, out var resource) == true)
         {
             return resource!;
         }
@@ -22,5 +30,29 @@ public class NameToResourceConverter : IValueConverter
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
+    }
+}
+
+
+public class NameToResourceConverterExtension
+{
+    public NameToResourceConverterExtension()
+    {
+        
+    }
+
+    public IValueConverter ProvideValue(IServiceProvider serviceProvider)
+    {
+        var anchor = GetFirstParent<StyledElement>() ??
+                    GetFirstParent<IResourceProvider>() ??
+                    (object?)GetFirstParent<IResourceHost>();
+
+        return new NameToResourceConverter(anchor as IResourceHost);
+
+        T? GetFirstParent<T>() where T : class
+        {
+            return (serviceProvider.GetService(typeof(IAvaloniaXamlIlParentStackProvider)) as IAvaloniaXamlIlParentStackProvider)?
+                .Parents.OfType<T>().FirstOrDefault();
+        }
     }
 }
