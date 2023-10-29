f42d62832aad4f65575644fdedeb334fb85accfe
Daniel Keenan <dfkeenan@gmail.com>
2023-10-29 11:26:03 +1000

    Add SimpleShellTheme

---
+++ Source/Kaigara.Core/Themes/SimpleShellTheme.axaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace Kaigara.Themes;
+public partial class SimpleShellTheme : Styles
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SimpleShellTheme"/> class.
+    /// </summary>
+    /// <param name="sp">The parent's service provider.</param>
+    public SimpleShellTheme(IServiceProvider? sp = null)
+    {
+        AvaloniaXamlLoader.Load(sp, this);
+    }
+}
--- Source/Kaigara.Core/XmlnsDefinitions.cs
+++ Source/Kaigara.Core/XmlnsDefinitions.cs
@@ -1,3 +1,4 @@
 ﻿using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/dfkeenan/Kaigara", "Kaigara.Shell")]
+[assembly: XmlnsDefinition("https://github.com/dfkeenan/Kaigara", "Kaigara.Themes")]
